3ec3697510ef3968d6874c3577136621b96547f6
lone02wolf <singh.mohan0265@gmail.com>
2023-10-13 16:14:57 +0530

    Category get API Query resolve

--- routes/hotel.router.js
+++ routes/hotel.router.js
@@ -6,7 +6,7 @@ const Hotel = require("../model/hotel.model")
 router.route("/")
     .get(async (req, res) => {
         try {
-            const hotels = await Hotel.find({});
+            const hotels = await Hotel.find(req.query);
             hotels ? res.json(hotels) : res.status(404).json({msg: "No data Found"});
         } catch (error) {
             console.log(error)
